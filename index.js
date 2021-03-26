e77689556909c63c8cfc32d95aa65ca188bbdb15
Jesse Brown <Brownjesse8895@yahoo.com>
2021-03-26 16:40:36 -0500

    commit assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function  destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const kitty = [...cats, name]
+    return kitty
+}
+
+function prependCat(name){
+    const kitty = [name, ...cats]
+    return kitty
+}
+
+function  removeLastCat(){
+    const kitty = cats.slice(0, 2)
+    return kitty 
+}
+
+function removeFirstCat(){
+    const kitty = cats.slice(1)
+    return kitty
+}
+
+
+    
+
+
+
+
+
+
+ 
